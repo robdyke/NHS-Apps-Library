b8810be0a8e9ca598a21ddfaa210bca2f6496871
Rob Dyke <robdyke@gmail.com>
2020-10-31 16:45:42 +0000

    It works!

--- scrapy/appstore/appstore/spiders/apps.py
+++ scrapy/appstore/appstore/spiders/apps.py
@@ -1,6 +1,5 @@
 import scrapy
-
-
+from urllib.parse import urlparse
 class AppsSpider(scrapy.Spider):
     name = 'apps'
     start_urls = ['https://www.nhs.uk/apps-library/'
@@ -23,9 +22,25 @@ class AppsSpider(scrapy.Spider):
     
         for app in response.css('div.aaw-app-details.nshuk-o-grid>div'):
 
+            # if 'apple' in response.xpath('//*[@id="main-content"]/div[1]/div/div[3]/div/a/@href'): appleurl = response.xpath('//*[@id="main-content"]/div[1]/div/div[3]/div/a/@href')
+
+            app_name = app.css('div.aaw-app-details-head > div.aaw-app-details-head__description > h1::text').get()
+            app_categories = app.css('div.aaw-app-details-head > div.aaw-app-details-head__description > p > a::text').getall()
+            app_bio = app.css('div.rich-text > p::text').getall()
+            app_stores = response.xpath('//*[@id="main-content"]/div[1]/div/div[3]/div/a/@href').getall()
+            app_applestore = response.xpath('//a[contains(@href, "apple")]/@href').get()
+            app_playstore = response.xpath('//a[contains(@href, "google")]/@href').get()
+            if app_playstore:
+                app_playparsed = urlparse(response.xpath('//a[contains(@href, "google")]/@href').get())
+                app_playid = app_playparsed.query
+
             yield {
-                'name': app.css('div.aaw-app-details-head > div.aaw-app-details-head__description > h1::text').get(),
-                'category': app.css('div.aaw-app-details-head > div.aaw-app-details-head__description > p > a::text').getall(),
-                'bio': app.css('div.rich-text > p::text').get(),
-                'store': response.xpath('//*[@id="main-content"]/div[1]/div/div[3]/div/a/@href').getall()
+                'app_name': app_name,
+                'app_categories': app_categories,
+                'app_bio': app_bio,
+                'app_stores': app_stores,
+                'app_applestore': app_applestore,
+                'app_playstore': app_playstore,
+                'app_playparsed': app_playparsed,
+                'app_playid': app_playid
             }
